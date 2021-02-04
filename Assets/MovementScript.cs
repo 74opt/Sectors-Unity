145316141e8ba65384242a42f9dc13479f6b2088
74opt <potatomatt0505@gmail.com>
2021-02-04 17:10:48 -0500

    Got a tempo timer working w/ vars

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -8,8 +8,9 @@ public class MovementScript : MonoBehaviour {
 
     float horizontal;
     float vertical;
-
-    float playerSpeed = 5.0f;
+    float playerSpeed = 7.0f;
+    float dashForce;
+    public static bool activeDash = true;
 
     // Start is called before the first frame update
     void Start() {
@@ -18,13 +19,23 @@ public class MovementScript : MonoBehaviour {
 
     // Update is called once per frame
     void Update() { // Retrieves inputs
-        horizontal = Input.GetAxis("Horizontal");
+        horizontal = Input.GetAxis("Horizontal");  // TODO: Change movement system to use AddForce rather than changing velocity
         vertical = Input.GetAxis("Vertical");
+      
+        if (Input.GetKey(KeyCode.LeftShift) & activeDash & TempoScript.tempoActive4) {
+            dashForce = 5000.0f;
+            activeDash = false;
+        }
     }
 
     // FixedUpdate can run once, zero, or several times per frame, depending on how many physics frames per second are set in the time settings, and how fast/slow the framerate is.
     void FixedUpdate() {
         body.velocity = new Vector2(horizontal * playerSpeed, vertical * playerSpeed); // Moves player
+
+        if (dashForce != 0) {
+            body.AddForce(transform.right * dashForce);
+            dashForce = 0;
+        }
     }
 
 }
---
+++ Assets/TempoScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class TempoScript : MonoBehaviour {
+    float tempo = 120;
+    float frequency;
+    public static bool tempoActive4;
+    public static bool tempoActive2;
+    public static bool tempoActive1;
+
+    // Start is called before the first frame update
+    void Start() {
+        frequency = (float) Math.Pow(tempo/60/.25, -1);
+        Debug.Log(frequency);
+    }
+
+    void TrueQuarter() {
+        tempoActive4 = true;
+    }
+
+    void TrueHalf() {
+        tempoActive2 = true;
+    }
+
+    void TrueWhole() {
+        tempoActive1 = true;
+    }
+
+    void FalseQuarter() {
+        tempoActive4 = false;
+    }
+
+    void FalseHalf() {
+        tempoActive2 = false;
+    }
+
+    void FalseWhole() {
+        tempoActive1 = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        InvokeRepeating("TrueQuarter", 0.0f, frequency);
+        InvokeRepeating("TrueHalf", 0.0f, frequency * 2);
+        InvokeRepeating("TrueWhole", 0.0f, frequency * 4);
+
+        InvokeRepeating("FalseQuarter", 0.0f, frequency + .0167f);
+        InvokeRepeating("FalseHalf", 0.0f, frequency * 2 + .0167f);
+        InvokeRepeating("FalseWhole", 0.0f, frequency * 4 + .0167f);
+
+        Debug.Log(Convert.ToString(tempoActive1) + Convert.ToString(tempoActive2) + Convert.ToString(tempoActive4));
+    }
+}
