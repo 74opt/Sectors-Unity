794b22256f742a5b62f03b01c2f6241d5c649216
74opt <potatomatt0505@gmail.com>
2021-02-15 17:12:20 -0500

    Initial Commit

---
+++ Assets/Sprites/Player/ShootingScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour {
+    public Rigidbody2D projectile;
+    public const float bulletSpeed = 30.0f;
+    Rigidbody2D projectileInstance;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            projectileInstance = Instantiate(projectile, transform.position, transform.rotation);
+            //projectileInstance.AddForce(new Vector2(mouse) * bulletSpeed);
+        }
+    }
+}
---
+++ Assets/WeaponScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour {
+    Weapon testWeapon = new Weapon();
+    
+    // Start is called before the first frame update
+    void Start() {
+        testWeapon.name = "fish";
+        testWeapon.damage = 23;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        testWeapon.testMethod2(" aaa");
+    }
+}
+
+public class Weapon {
+    public string name;
+    public float damage;
+
+    public int testMethod(int x, int y) {
+        int num = x * y;
+        return num;
+    }
+
+    public void testMethod2(string test) {
+        Debug.Log(name + test);
+    }
+}
