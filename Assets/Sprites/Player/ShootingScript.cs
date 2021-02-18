96b8d4550113d5bc5f575c889f3a1f2a053fe7d1
74opt <potatomatt0505@gmail.com>
2021-02-18 15:41:22 -0500

    Got groundwork for shooting

--- Assets/Sprites/Player/ShootingScript.cs
+++ Assets/Sprites/Player/ShootingScript.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class ShootingScript : MonoBehaviour {
     public Rigidbody2D projectile;
-    public const float bulletSpeed = 30.0f;
+    public const float bulletSpeed = 20.0f;
+    public Transform aim;
     Rigidbody2D projectileInstance;
+    public Collider2D collision;
+    public const float bulletTimerConst = 1.0f;
+    public float bulletTimer;
+
+    void Start() {
+        bulletTimer = bulletTimerConst;
+    }
 
     // Update is called once per frame
     void Update() {
+
+
         if (Input.GetMouseButtonDown(0)) {
-            projectileInstance = Instantiate(projectile, transform.position, transform.rotation);
-            //projectileInstance.AddForce(new Vector2(mouse) * bulletSpeed);
+            projectileInstance = Instantiate(projectile, transform.position + new Vector3(.1f, .2f, 0), transform.rotation);
+            projectileInstance.AddForce(aim.right * bulletSpeed, ForceMode2D.Impulse);
         }
     }
 }
