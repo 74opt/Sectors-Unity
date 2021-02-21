ff50d32203edb8ca8bd42eea38c948fbc8164297
74opt <potatomatt0505@gmail.com>
2021-02-21 14:29:33 -0500

    Added sprite + script for enemy

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+    Rigidbody2D body;
+    public Transform playerTransform;
+    const float enemySpeed = 1.7f;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    void FixedUpdate() {
+        // looks towards player
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, playerTransform.position - transform.position);
+
+        // moves towards player
+        transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, enemySpeed * Time.deltaTime);
+    }
+}
