74bd2336b7624671a608e1a257c01aa1514b0d0a
74opt <potatomatt0505@gmail.com>
2021-03-06 16:25:36 -0500

    Changed some things

---
+++ Assets/HealthBarScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarScript : MonoBehaviour { // this is just to store a function to be used
+    //public Transform scale;
+    //public float health;
+    //public float healthMax;
+
+    public static void healthScale(float healthCurrent, float healthMax, Transform healthBar) {
+        healthBar.localScale = new Vector3(healthCurrent / healthMax, 1, 1);
+    }
+}
