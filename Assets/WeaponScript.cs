17a20fcd5c7f8ddf788d6120d7c295456914fc3b
74opt <potatomatt0505@gmail.com>
2021-03-06 16:20:13 -0500

    tried some tempo things, didn't work

--- Assets/WeaponScript.cs
+++ Assets/WeaponScript.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponScript : MonoBehaviour {
-    Weapon testWeapon = new Weapon();
-    
+    Weapon testWeapon = new Weapon("test", 23);
+
     // Start is called before the first frame update
     void Start() {
-        testWeapon.name = "fish";
-        testWeapon.damage = 23;
+        
     }
 
     // Update is called once per frame
     void Update() {
-        testWeapon.testMethod2(" aaa");
+        //testWeapon.TestMethod2(" aaa");
     }
 }
 
@@ -21,12 +20,17 @@ public class Weapon {
     public string name;
     public float damage;
 
-    public int testMethod(int x, int y) {
+    public Weapon(string weaponName, float weaponDamage) {
+        name = weaponName;
+        damage = weaponDamage;
+    }
+
+    public int TestMethod(int x, int y) {
         int num = x * y;
         return num;
     }
 
-    public void testMethod2(string test) {
+    public void TestMethod2(string test) {
         Debug.Log(name + test);
     }
 }
